7a5b51cb372b1241355e655ca5c1c22575daa793
Jay Amdavadi <jayamdavadi2003@gmail.com>
2025-08-29 20:16:35 -0400

    Used Openai Api for Speech to text transfer

--- stt.py
+++ stt.py
@@ -1,6 +1,20 @@
-import whisper
+# import whisper
 
-model = whisper.load_model("large-v2")
-## Used for testing New_Year_Resolution.mp3
-result = model.transcribe(audio = "audios/New_Year_Resolution.mp3", language = "en",task="translate")
-print(result["text"])
+# model = whisper.load_model("large-v2")
+# ## Used for testing New_Year_Resolution.mp3
+# # result = model.transcribe(audio = "audios/New_Year_Resolution.mp3", language = "en",task="translate")
+# result1 = model.transcribe(audio = "audios/sample.mp3", language = "en",task="translate")
+# print(result1["text"])
+
+
+from openai import OpenAI
+
+client = OpenAI()
+audio_file= open("audios/sample.mp3", "rb")
+
+transcription = client.audio.transcriptions.create(
+    model="gpt-4o-transcribe", 
+    file=audio_file
+)
+
+print(transcription.text)
